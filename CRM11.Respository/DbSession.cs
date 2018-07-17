de3f7d9182b0fd6154aa8e9df6ebb98fc4a06723
jingchenxi <jingchenzy@qq.com>
2018-07-17 13:43:04 +0800

    权限管理的提交

---
+++ CRM11.Respository/DbSession.cs
@@ -0,0 +1,25 @@
+﻿using CRM11.IRespository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM11.Respository
+{
+    public partial class DbSession:IDbSession
+    {
+      
+        /// <summary>
+        /// 调用EF容器 批量执行sql 语句
+        /// </summary>
+        /// <returns></returns>
+        public int SaveChanges()
+        {
+            //从线程中获取DbContext对象
+            return EFFactory.GetEntityContext().SaveChanges();
+
+        }
+
+    }
+}
