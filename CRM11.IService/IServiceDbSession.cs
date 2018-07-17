c59080fd44af67a4d1d03ead50d095aac071be8f
jingchenxi <jingchenzy@qq.com>
2018-07-17 13:39:48 +0800

    权限管理的提交

---
+++ CRM11.IService/IServiceDbSession.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM11.IService
+{
+    public partial interface IServiceDbSession
+    {
+        int SaveChanges();
+    }
+}
