ed71974c7b1e832e84160cbe36d35d0af847a688
jingchenxi <jingchenzy@qq.com>
2018-07-17 13:49:10 +0800

    权限管理的提交

---
+++ CRM11.UI/Helper/BaseController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRM11.UI.Helper
+{
+    public class BaseController:Controller
+    {
+        public OperationContext OperContext
+        {
+            get
+            {
+                return OperationContext.Current;
+            }
+        }
+    }
+}
