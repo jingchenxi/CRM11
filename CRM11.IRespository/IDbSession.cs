fdc4f61386f5d22ead82c4934d0d993a174d6044
jingchenxi <jingchenzy@qq.com>
2018-07-17 13:36:34 +0800

    权限管理的提交

---
+++ CRM11.IRespository/IDbSession.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM11.IRespository
+{
+    public partial interface IDbSession
+    {
+        /// <summary>
+        /// 1.0 调用EF容器 批量 执行 sql语句
+        /// </summary>
+        /// <returns></returns>
+        int SaveChanges();
+    }
+}
