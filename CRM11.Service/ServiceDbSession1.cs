9a6b7b0bbd7f26691cbdcb03e4664e5c6165c9a6
jingchenxi <jingchenzy@qq.com>
2018-07-17 13:46:50 +0800

    权限管理的提交

---
+++ CRM11.Service/ServiceDbSession1.cs
@@ -0,0 +1,26 @@
+﻿using CRM11.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM11.Service
+{
+    public partial class ServiceDbSession:CRM11.IService.IServiceDbSession
+    {
+        public int SaveChanges()
+        {
+            IRespository.IDbSession dbSession =  System.Runtime.Remoting.Messaging.CallContext.GetData(typeof(IRespository.IDbSession).FullName) as IRespository.IDbSession;
+            if (dbSession == null)
+            {
+                dbSession = DI.GetObject<IRespository.IDbSession>("dalSession");
+                System.Runtime.Remoting.Messaging.CallContext.SetData(typeof(IRespository.IDbSession).FullName, dbSession);
+            }
+
+            return  dbSession.SaveChanges();
+
+            
+        }
+    }
+}
